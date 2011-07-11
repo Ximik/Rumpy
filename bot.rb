b673b6ea61dbf326f37c95d8ab836a09c8438ea7
Alex Tsokurov <me@ximik.net>
2011-07-11 20:34:32 +0000

    2nd

--- bot.rb
+++ bot.rb
@@ -3,13 +3,13 @@
 require 'rubygems'
 require 'xmpp4r/client'
 require 'xmpp4r/roster'
+require 'active_record'
 
 class Bot
   include Jabber
   attr_accessor :backend_func
 
   def initialize
-#    Jabber::debug = true
     @jid = JID::new 'test@ximik.net'
     @password = 'test'
     @client = Client.new @jid
@@ -38,7 +38,22 @@ class Bot
 
   def start_subscription_callback
     @roster.add_subscription_request_callback do |item, presence|
-       puts item+' '+presence
+      if presence.type == :subscribe and item.nil?
+        jid = presence.from
+        @roster.accept_subscription jid
+        @client.send Presence.new.set_type(:subscribe).set_to(jid)
+        send_msg "hello", jid
+      end
+    end
+    @roster.add_subscription_callback do |item, presence|
+      case presence.type
+      when :unsubscribed
+        item.remove
+      when :unsubscribe
+        remove_jid item.jid.node
+      when :subscribed
+        add_jid item.jid.node
+      end
     end
   end
   
@@ -47,6 +62,20 @@ class Bot
        puts m
     end
   end
+
+  def send_msg(text, destination)
+    msg = Message::new
+    msg.to = destination
+    msg.body = text
+    @client.send msg
+  end
+
+  def add_jid(jid)
+  end
+
+  def remove_jid(jid)
+  end
+
 end
 bot = Bot.new
 bot.start
