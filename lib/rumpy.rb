466fc108b9639f74f155a4285a6e0915e50437b2
Michael Pogoda <mpogoda@lavabit.com>
2011-07-15 18:40:36 +0300

    :3

--- lib/rumpy.rb
+++ lib/rumpy.rb
@@ -6,27 +6,27 @@ require 'active_record/validations'
 
 module Rumpy
 
-
   def self.start(botclass)
     pid = fork do
       botclass.new.start
     end
-    File.open(botclass.to_s + '.pid', 'w') do |file|
+    File.open(botclass.to_s.downcase + '.pid', 'w') do |file|
       file.puts pid
     end
     Process.detach pid
   end
 
   def self.stop(botclass)
-    File.open(botclass.to_s + '.pid') do |file|
+    File.open(botclass.to_s.downcase + '.pid') do |file|
       Process.kill :TERM, file.gets.strip.to_i
     end
+    File.unlink botclass.to_s.downcase + '.pid'
   end
 
   module Bot
-  include Jabber
+    include Jabber
 
-  def start
+    def start
       init
       connect
       clear_users
@@ -35,72 +35,74 @@ module Rumpy
       @client.send Presence.new
       Thread.new do
         loop do
-          send_msg backend_func
+          backend_func().each do |result|
+            send_msg *result
+          end
         end
       end if self.respond_to? :backend_func
       Thread.stop
-  end
+    end
 
-  private
+    private
 
-  def init
-    xmppconfig  = YAML::load File.open(@config_path + '/xmpp.yml')
-    @lang       = YAML::load File.open(@config_path + '/lang.yml')
-    @jid        = JID.new xmppconfig['jid']
-    @password   = xmppconfig['password']
-    @client     = Client.new @jid
-    if not @models_path.nil? then
-      dbconfig    = YAML::load File.open(@config_path + '/database.yml')
-      ActiveRecord::Base.establish_connection dbconfig
-      Dir[@models_path].each do |file|
-        Object.require file
+    def init
+
+      xmppconfig  = YAML::load_file @config_path + '/xmpp.yml'
+      @lang       = YAML::load_file @config_path + '/lang.yml'
+      @jid        = JID.new xmppconfig['jid']
+      @password   = xmppconfig['password']
+      @client     = Client.new @jid
+
+      if @models_path then
+        dbconfig  = YAML::load_file @config_path + '/database.yml'
+        ActiveRecord::Base.establish_connection dbconfig
+        Dir[@models_path].each do |file|
+          self.class.require file
+        end
+      end
+
+      @main_model = Object.const_get @main_model.to_s.capitalize
+      def @main_model.find_by_jid(jid)
+        super jid.strip.to_s
+      end
+
+      @mutexes = Hash.new do |h, k|
+        h[k] = Mutex.new
       end
     end
-    @main_model = Object.const_get @main_model.to_s.capitalize
-    def @main_model.find_by_jid(jid)
-      super jid.strip.to_s
-    end
-    @mutexes = Hash.new do |h, k|
-      h[k] = Mutex.new
-    end
-  end
 
-  def connect
-    @client.connect
-    @client.auth @password
-    @roster = Roster::Helper.new @client
-    @roster.wait_for_roster
-  end
-
-  def clear_users
-    @main_model.find_each do |user|
-      items = @roster.find user.jid
-      user.destroy if items.count != 1
+    def connect
+      @client.connect
+      @client.auth @password
+      @roster = Roster::Helper.new @client
+      @roster.wait_for_roster
     end
-    @roster.items.each do |jid, item|
-      user = @main_model.find_by_jid jid
-      if user.nil? then
-        item.remove
-        next
+
+    def clear_users
+      @main_model.find_each do |user|
+        items = @roster.find user.jid
+        user.destroy if items.count != 1
       end
-      if item.subscription != :both then
-        item.remove
-        user.remove unless user.nil?
+      @roster.items.each do |jid, item|
+        user = @main_model.find_by_jid jid
+        if user.nil? then
+          item.remove
+          next
+        elsif item.subscription != :both then
+          item.remove
+          user.remove
+        end
       end
     end
-  end
 
-  def start_subscription_callback
-    @roster.add_subscription_request_callback do |item, presence|
-      Thread.new do
+    def start_subscription_callback
+      @roster.add_subscription_request_callback do |item, presence|
         jid = presence.from
         @roster.accept_subscription jid
         @client.send Presence.new.set_type(:subscribe).set_to(jid)
         send_msg jid, @lang['hello']
       end
-    end
-    @roster.add_subscription_callback do |item, presence|
-      Thread.new do
+      @roster.add_subscription_callback do |item, presence|
         case presence.type
         when :unsubscribed
           item.remove
@@ -112,12 +114,10 @@ module Rumpy
         end
       end
     end
-  end
 
-  def start_message_callback
-    @client.add_message_callback do |msg|
-      if msg.type != :error and msg.body and msg.from then
-        Thread.new do
+    def start_message_callback
+      @client.add_message_callback do |msg|
+        if msg.type != :error and msg.body and msg.from then
           if user = @main_model.find_by_jid(msg.from) then
             pars = parser_func msg.body
             message = ""
@@ -133,25 +133,23 @@ module Rumpy
         end
       end
     end
-  end
 
-  def send_msg(destination, text)
-    return if destination.nil? or text.nil?
-    msg = Message.new destination, text
-    msg.type = :chat
-    @client.send msg
-  end
+    def send_msg(destination, text)
+      return if destination.nil? or text.nil?
+      msg = Message.new destination, text
+      msg.type = :chat
+      @client.send msg
+    end
 
-  def add_jid(jid)
-    user = @main_model.new
-    user.jid = jid.strip.to_s
-    user.save
-  end
+    def add_jid(jid)
+      user = @main_model.new
+      user.jid = jid.strip.to_s
+      user.save
+    end
 
-  def remove_jid(jid)
-    user = @main_model.find_by_jid jid
-    user.destroy unless user.nil?
-  end
-
+    def remove_jid(jid)
+      user = @main_model.find_by_jid jid
+      user.destroy unless user.nil?
+    end
   end
 end
